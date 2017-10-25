c3440f679dcfecc770b5c9de27374c82be8ba491
Damian Kalinowski <damiikalii@gmail.com>
2017-10-25 18:18:45 +0200

    Connect to server from admin app (react)

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import io from 'socket.io-client';
+
+let socket = io('http://localhost:6777');
+
 class App extends Component {
   render() {
     return (
