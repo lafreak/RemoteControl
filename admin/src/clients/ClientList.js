ab83a9e044f93b0752e0b5b67b66be3bc6eb815a
Damian Kalinowski <damiikalii@gmail.com>
2017-11-14 17:48:20 +0100

    Client avatar

--- admin/src/clients/ClientList.js
+++ admin/src/clients/ClientList.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import List, { ListItem, ListItemText } from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
 
 export default class ClientList extends React.Component {
 	render () {
@@ -13,6 +14,7 @@ export default class ClientList extends React.Component {
 						key={c.id}
 						style={c.id === this.props.selectedId ? {backgroundColor: '#E0E0E0'} : null}
 						onClick={() => this.props.onClientChange(c.id)}>
+						<Avatar>{c.id.charAt(0)}</Avatar>
 						<ListItemText primary={c.id} />
 					</ListItem>
 				)
