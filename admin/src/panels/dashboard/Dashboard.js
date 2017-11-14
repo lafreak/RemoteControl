5cd319d2862d2d8f6fc26487bd73eff335b03508
Damian Kalinowski <damiikalii@gmail.com>
2017-11-14 17:54:00 +0100

    place for information

--- admin/src/panels/dashboard/Dashboard.js
+++ admin/src/panels/dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Map from './Map';
 
-import Card, { CardHeader, CardMedia } from 'material-ui/Card';
+import Card, { CardHeader, CardContent, CardMedia } from 'material-ui/Card';
 import Button from 'material-ui/Button';
 import Divider from 'material-ui/Divider';
 
@@ -12,6 +12,9 @@ export default class Dashboard extends React.Component {
       <Card style={{marginBottom: 20}}>
         <CardHeader title="Dashboard" />
         <Divider />
+        <CardContent>
+          {this.props.client.id}
+        </CardContent>
         <CardMedia>
         	<Map center={{ lat: this.props.client.lat, lng: this.props.client.lng }} height={200} />
         </CardMedia>
