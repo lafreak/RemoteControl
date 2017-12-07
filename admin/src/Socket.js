2cd3fbb31cefe7837c94c1ad2a2a9985dbf0e238
Damian Kalinowski <damiikalii@gmail.com>
2017-12-07 15:39:41 +0100

    Screen sharing frontend with controls

--- admin/src/Socket.js
+++ admin/src/Socket.js
@@ -1,4 +1,5 @@
 import io from 'socket.io-client';
 //let socket = io('http://145.239.82.140:6777');
-let socket = io('localhost:6777');
+//let socket = io('localhost:6777');
+let socket = io('192.168.137.1:6777');
 export { socket };
